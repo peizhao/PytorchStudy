f70ded1d368619c241375197cd6e2f0affedc66a
lipz <lipz@hiscene.com>
2018-04-11 17:15:56 +0800

    add the input and output shape for lstm

--- basic/rnn/lstm.py
+++ basic/rnn/lstm.py
@@ -24,3 +24,11 @@ print(net.weight_ih_l2.shape)
 print(net.bias_ih_l2.shape)
 print(net.weight_hh_l2.shape)
 print(net.bias_hh_l2.shape)
+
+
+print("The input and output for LSTM ...")
+print("input shape is (3,1,50)")
+x = Variable(torch.randn(3,1,50)) # input
+net = nn.LSTM(50,1)
+lstm_seq_out, _ = net(x)
+print("lstm_seq_out shape is {}".format(lstm_seq_out.shape))
